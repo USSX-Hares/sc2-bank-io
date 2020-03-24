5def81354a7b8849e2eecdac8898d726ed1ec3ab
Peter Zaitcev / USSX-Hares <ussx-hares@yandex.ru>
2020-03-24 08:40:17 +0300

    Python: Reworked EventsBank API

--- bot-client/starcraft_io/events_api/_bank.py
+++ bot-client/starcraft_io/events_api/_bank.py
@@ -1,9 +1,9 @@
 from dataclasses import dataclass, field
-from types import MappingProxyType
 from typing import *
 
-from ..bank_api import Bank, AbstractBank
-from ..util import MappingProxy
+from ..abstract import *
+from ..bank_api import *
+from ..util import filter_out
 from ._model import *
 from ._events import *
 
@@ -12,44 +12,43 @@ class EventsBank(AbstractBank):
     bank: Bank = field(repr=False)
     meta: MetaData = field(init=False)
     
-    @property
-    def events(self) -> Iterable[Event]:
-        return self._events.values()
-    @property
-    def requests(self) -> MappingProxy[str, Event]:
-        return self._proxies[MessageType.Request]
-    @property
-    def responses(self) -> MappingProxy[str, Event]:
-        return self._proxies[MessageType.Response]
-    
     def __post_init__(self):
-        self._events = dict()
-        self._proxies = dict()
-        
-        for message_type in MessageType: # type: MessageType
-            self._proxies[message_type] = self._get_proxy(message_type)
-        
         self.reload()
     
-    def _get_proxy(self, message_type: MessageType) -> MappingProxy[str, Event]:
-        return MappingProxy(lambda ev_id: self._events[message_type, ev_id], lambda: (e.event_id for e in self._events.values() if e.message_type == message_type))
-    def _check_meta(self):
-        if (self.meta.protocol_version != protocol_version):
-            raise ValueError(f"Unsupported protocol version: {self.meta.protocol_version}, expected: {protocol_version}")
+    def _get_event(self, event_id: str, message_type: MessageType, *, pop: bool = False) -> Event:
+        getter: Callable[[str], BankSection]
+        if (pop):
+            getter = self.bank.pop
+        else:
+            getter = self.bank.get
+        
+        event_data = EventData.from_dict(getter(Literals.get_event_key(event_id, message_type)).as_dict())
+        event_type = EVENT_MAPPING[event_data.event_type]
+        event = event_type(event_id=event_id, message_type=message_type, event_data=event_data)
+        return event
     
     def add_event(self, event: Event):
-        raise NotImplementedError
+        self.bank.set_value(Literals.get_event_key(event.event_id, event.message_type), filter_out(event.event_data.to_dict(encode_json=True)))
+        ids_list = self.bank.get_or_create(Literals.get_event_list_key(event.message_type))
+        ids_list.set_value(event.event_id, event.event_id)
+    
+    def get_event(self, event_id: str, message_type: MessageType) -> Event:
+        return self._get_event(event_id, message_type, pop=False)
+    def pop_event(self, event_id: str, message_type: MessageType) -> Event:
+        return self._get_event(event_id, message_type, pop=True)
+    
+    def event_ids(self, message_type: MessageType, *, pop: bool = False) -> Iterable[str]:
+        getter: Callable[[str], BankSection]
+        if (pop):
+            getter = self.bank.pop
+        else:
+            getter = self.bank.get
+        
+        return getter(Literals.get_event_list_key(message_type), dict()).keys()
     
     def reload(self):
         self.bank.reload()
-        self.meta = MetaData.from_dict(self.bank['meta'].as_dict())
-        self._check_meta()
-        
-        for message_type in MessageType: # type: MessageType
-            for event_id in self.bank.get(f'events.{message_type.value}s', dict()):
-                event_data = EventData.from_dict(self.bank[f'{message_type.value}.{event_id}'].as_dict())
-                event = Event(event_id, message_type, event_data)
-                self._events[message_type, event_id] = event
+        self.meta = MetaData.from_dict(self.bank[Literals.MetaSectionKey.value].as_dict())
     
     def save(self):
         self.bank.save()
